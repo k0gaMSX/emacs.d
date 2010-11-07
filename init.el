c6c17491391b74fdc42977aa11cefd779dd44b0f
Roberto E. Vargas Caballero <k0ga@shike2.com>
2010-11-07 14:13:04 +0100

    Setting size depending of resolution

--- init.el
+++ init.el
@@ -1068,3 +1068,26 @@
 (eval-after-load "sql"
   ;; (load-library "sql-complete")          ; I need know how I can use it
   (load-library "sql-indent"))
+
+
+;; Adjust Emacs size according to resolution
+
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+      (progn
+        ;; use 130 char wide window for largeish displays
+        ;; and smaller 80 column windows for smaller displays
+        ;; pick whatever numbers make sense for you
+        (if (> (x-display-pixel-width) 1280)
+            (add-to-list 'default-frame-alist (cons 'width 140))
+          (add-to-list 'default-frame-alist (cons 'width 80)))
+        ;; for the height, subtract a couple hundred pixels
+        ;; from the screen height (for panels, menubars and
+        ;; whatnot), then divide by the height of a char to
+        ;; get the height we want
+        (add-to-list 'default-frame-alist
+                      (cons 'height (/ (- (x-display-pixel-height) 150)
+                                       (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
