217ee0571e736198873580deb979f5edaac259f6
Roberto E. Vargas Caballero <k0ga@shike2.com>
2011-07-21 21:47:45 +0200

    Adding support for clang in auto-complete for C/C++ modes
    clang allows sense completion in a better way than semantic does it.
    It is faster and accuracy.

--- init.el
+++ init.el
@@ -511,15 +511,31 @@ save the pointer marker if tag is found"
 
 (require 'auto-complete)
 (require 'auto-complete-config)
+(require 'auto-complete-clang)
+(require 'cc-mode)
+
+(defvar my-auto-complete-clangp nil
+  "Flag to indicate clang is present")
+
+(when (file-executable-p ac-clang-executable)
+  (setq my-auto-complete-clangp t))
+
 (ac-config-default)
 (global-auto-complete-mode t)
 
-(global-set-key [(control  return)]
-                (lambda nil
-                  (interactive)
-                  (auto-complete '(ac-complete-semantic
-                                   ac-complete-semantic-raw))))
 
+(defun my-sense-completion ()
+  "Performs a auto completions based in sense information
+taken from clang if it is installed, or in other case taken
+from semantic"
+  (interactive)
+  (if (and my-auto-complete-clangp c-buffer-is-cc-mode)
+      (auto-complete '(ac-source-clang))
+    (auto-complete '(ac-source-semantic
+                     ac-source-semantic-raw))))
+
+
+(global-set-key [(control return)] 'my-sense-completion)
 (define-key ac-menu-map "\C-n" 'ac-next)
 (define-key ac-menu-map "\C-p" 'ac-previous)
 (ac-flyspell-workaround)
