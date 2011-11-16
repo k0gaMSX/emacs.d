1d40a1be6b1422ff16bbf35bca2c34490dbfb959
Roberto E. Vargas Caballero <revargas.caballero@gmail.com>
2011-11-16 11:06:14 +0100

    Added marmalade elpa repository
    Non official repository with newer version of a lot of packets

--- init.el
+++ init.el
@@ -1080,6 +1080,7 @@ from semantic"
 (setq package-archives
       '(("ELPA" . "http://tromey.com/elpa/")
 	("gnu" . "http://elpa.gnu.org/packages/")
+	("marmalade" . "http://marmalade-repo.org/packages/")
 	("SC"  . "http://joseito.republika.pl/sunrise-commander/")))
 
 
