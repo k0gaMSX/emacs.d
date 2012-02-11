7223c80a24109bcc685c92ee2e426091139b4594
Roberto E. Vargas Caballero <revargas.caballero@gmail.com>
2012-02-11 10:34:14 +0100

    Moved windows configuration to different file

--- init.el
+++ init.el
@@ -238,7 +238,8 @@
   (load-file "my_gud.el")
   (load-file "my_lisp.el")
   (load-file "my_yasnippet.el")
-  (load-file "my_jump.el"))
+  (load-file "my_jump.el")
+  (load-file "my_window.el"))
 
 
 
@@ -316,42 +317,6 @@
     (require 'cygwin-mount)
     (cygwin-mount-activate)))
 
-
-
-
-
-
-
-
-;; ****************************************************************************
-;; window stuff
-;; ****************************************************************************
-
-
-(defun select-next-window ()
-  "Switch to the next window"
-  (interactive)
-  (select-window (next-window)))
-
-(defun select-previous-window ()
-  "Switch to the previous window"
-  (interactive)
-  (select-window (previous-window)))
-
-
-(global-set-key [(control shift up)] 'enlarge-window)
-(global-set-key [(control shift down)] 'shrink-window)
-(global-set-key [(control shift left)] 'enlarge-window-horizontally)
-(global-set-key [(control shift right)] 'shrink-window-horizontally)
-
-
-
-(global-set-key [(control shift up)] 'enlarge-window)
-(global-set-key [(control shift down)] 'shrink-window)
-(global-set-key [(control shift left)] 'enlarge-window-horizontally)
-(global-set-key [(control shift right)] 'shrink-window-horizontally)
-(global-set-key [(meta right)] 'select-next-window)
-(global-set-key [(meta left)]  'select-previous-window)
 
 
 
