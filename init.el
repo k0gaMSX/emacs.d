3be9d75ae2b95d871afc6f53690451a616c9f5ab
Roberto E. Vargas Caballero <revargas.caballero@gmail.com>
2012-02-11 10:14:48 +0100

    Moved auto-complete configuration to different file

--- init.el
+++ init.el
@@ -232,7 +232,8 @@
   (load-file "my_flymake.el")
   (load-file "my_ecb.el")
   (load-file "my_cedet.el")
-  (load-file "my_doxymacs.el"))
+  (load-file "my_doxymacs.el")
+  (load-file "my_autocomplete.el"))
 
 
 
@@ -291,41 +292,6 @@
 
 
 
-;; ****************************************************************************
-;; Autocomplete modes
-;; ****************************************************************************
-
-
-(require 'auto-complete)
-(require 'auto-complete-config)
-(require 'auto-complete-clang)
-(require 'cc-mode)
-
-(defvar my-auto-complete-clangp nil
-  "Flag to indicate clang is present")
-
-(when (and ac-clang-executable (file-executable-p ac-clang-executable))
-  (setq my-auto-complete-clangp t))
-
-(ac-config-default)
-(global-auto-complete-mode t)
-
-
-(defun my-sense-completion ()
-  "Performs a auto completions based in sense information
-taken from clang if it is installed, or in other case taken
-from semantic"
-  (interactive)
-  (if (and my-auto-complete-clangp c-buffer-is-cc-mode)
-      (auto-complete '(ac-source-clang))
-    (auto-complete '(ac-source-semantic
-		     ac-source-semantic-raw))))
-
-
-(global-set-key [(control return)] 'my-sense-completion)
-(define-key ac-menu-map "\C-n" 'ac-next)
-(define-key ac-menu-map "\C-p" 'ac-previous)
-(ac-flyspell-workaround)
 
 
 
